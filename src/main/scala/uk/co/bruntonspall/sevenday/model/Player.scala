9144dd4d34028fe4ee44dc548ca7a3259e46add3
Michael Brunton-Spall <michael@brunton-spall.co.uk>
2013-03-12 09:04:30 +0000

    Add compositing of characters on the map

--- src/main/scala/uk/co/bruntonspall/sevenday/model/Player.scala
+++ src/main/scala/uk/co/bruntonspall/sevenday/model/Player.scala
@@ -1,6 +1,13 @@
 package uk.co.bruntonspall.sevenday.model
 
-case class Player(var x: Int, var y: Int)
+abstract class Mobile {
+  def x: Int
+  def y: Int
+  def glyph: Char
+  def htmlGlyph: String
+}
+
+case class PlayerMobile(x: Int, y: Int, glyph: Char, htmlGlyph: String) extends Mobile
 object Player {
-  lazy val character = Player(0, 7)
+  lazy val character = PlayerMobile(1, 13, '@', "@")
 }
--- src/main/scala/uk/co/bruntonspall/sevenday/servlets/DispatcherServlet.scala
+++ src/main/scala/uk/co/bruntonspall/sevenday/servlets/DispatcherServlet.scala
@@ -5,9 +5,11 @@ import uk.co.bruntonspall.sevenday.scalatra.TwirlSupport
 import uk.co.bruntonspall.sevenday.model._
 
 object Renderer {
-  def render(map: World, character: Player): Seq[Seq[String]] = {
-    for (row <- map.rows) yield {
-      for (cell <- row) yield cell.htmlGlyph
+  def render(map: World, character: List[Mobile]): Seq[Seq[String]] = {
+    for { y <- 0 to map.rows.length - 1 } yield {
+      for { x <- 0 to map.rows(y).length - 1 } yield {
+        character.find(c => c.x == x && c.y == y).map(_.htmlGlyph).getOrElse(map.getTileAt(x, y).htmlGlyph)
+      }
     }
   }
 }
@@ -18,7 +20,7 @@ class DispatcherServlet extends ScalatraServlet with TwirlSupport {
     // We'l worry about sessions, users and stuff tomorrow
     // For now, we'll assume one single player (me), and one world
     // I suspect we'll want a compositing renderer at some point
-    html.map.render(Renderer.render(World.map0, Player.character))
+    html.map.render(Renderer.render(World.map0, List(PlayerMobile(1, 13, '@', "@"))))
   }
 
 }
