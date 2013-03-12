cf255c90ba9623c07de4c490401381605a9cdbeb
Michael Brunton-Spall <michael@brunton-spall.co.uk>
2013-03-12 23:25:43 +0000

    Got it working, move the character via json

--- src/main/scala/uk/co/bruntonspall/sevenday/servlets/DispatcherServlet.scala
+++ src/main/scala/uk/co/bruntonspall/sevenday/servlets/DispatcherServlet.scala
@@ -3,6 +3,7 @@ package uk.co.bruntonspall.sevenday.servlets
 import org.scalatra.ScalatraServlet
 import uk.co.bruntonspall.sevenday.scalatra.TwirlSupport
 import uk.co.bruntonspall.sevenday.model._
+import cc.spray.json._
 
 object Renderer {
   def render(map: World, character: List[Mobile]): Seq[Seq[String]] = {
@@ -14,20 +15,32 @@ object Renderer {
   }
 }
 
+case class Action(character: Int, x: Int, y: Int)
+object ActionProtocol extends DefaultJsonProtocol {
+  implicit val actionFormat = jsonFormat3(Action)
+}
+
+import ActionProtocol._
+
 class DispatcherServlet extends ScalatraServlet with TwirlSupport {
+  val world = World.map0
+  var player = PlayerMobile(1, 13, '@', "@")
+  val characters = List(player)
 
   get("/") {
     // We'l worry about sessions, users and stuff tomorrow
     // For now, we'll assume one single player (me), and one world
     // I suspect we'll want a compositing renderer at some point
-    html.map.render(Renderer.render(World.map0, List(PlayerMobile(1, 13, '@', "@"))))
+    html.map.render(Renderer.render(world, List(player)))
   }
 
   post("/execute") {
-    contentType = "text/html"
-    <ul>{
-      params.keys.map(param => <li>{ param }</li>)
-    }</ul>
+    params.keys.map { println }
+    val actions = params("actions").asJson.convertTo[List[Action]]
+    contentType = "text/json"
+    actions.foreach { action =>
+      player = PlayerMobile(action.x, action.y, player.glyph, player.htmlGlyph)
+    }
   }
 
 }
--- src/main/webapp/js/app.js
+++ src/main/webapp/js/app.js
@@ -2,9 +2,10 @@ var actions = []
 
 $(function() {
   $('.execute').click(function() {
-    $.post('/execute', { "actions": [{character: 1, x:3, y:3}] }).done(function (data) {
+    $.post('/execute', { "actions": JSON.stringify([{character: 1, x:3, y:3}]) }).done(function (data) {
       console.log(data);
-      window.reload();
+      window.location.reload();
     });
   })
+
 });
