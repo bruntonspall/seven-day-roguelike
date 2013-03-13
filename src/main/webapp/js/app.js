40f51f4f5fe67def48c1470ffb0ec4d4722d4a79
Michael Brunton-Spall <michael@brunton-spall.co.uk>
2013-03-13 10:46:59 +0000

    Allow click to move

--- src/main/webapp/js/app.js
+++ src/main/webapp/js/app.js
@@ -1,11 +1,18 @@
-var actions = []
+var actions = [];
 
 $(function() {
   $('.execute').click(function() {
-    $.post('/execute', { "actions": JSON.stringify([{character: 1, x:3, y:3}]) }).done(function (data) {
+    $.post('/execute', { "actions": JSON.stringify(actions) }).done(function (data) {
       console.log(data);
       window.location.reload();
     });
   })
-
+ $('.glyph').click( function (ev) {
+     var cell = $(this);
+     var x = cell.data('x');
+     var y = cell.data('y');
+     console.log("Moving character 1 to "+x+","+y);
+     $('#cell-'+x+'-'+y).css({"color": "red"});
+     actions = [{character:1, x:x, y:y}]
+ })
 });
