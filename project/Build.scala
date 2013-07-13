c5b820714a7ceae7be5b28221d15b3cd59e6ffbd
Michael Brunton-Spall <michael@brunton-spall.co.uk>
2013-07-13 13:25:46 +0100

    Update build dependencies to remove wrox4j

--- project/Build.scala
+++ project/Build.scala
@@ -3,8 +3,6 @@ import Keys._
 import sbtappengine.Plugin.appengineSettings
 import com.typesafe.sbt.SbtScalariform.scalariformSettings
 import twirl.sbt.TwirlPlugin._
-import com.bowlingx.sbt.plugins.Wro4jPlugin._
-import Wro4jKeys._
 
 object sevenDayRoguelikeBuild  extends Build {
 
@@ -32,15 +30,14 @@ object sevenDayRoguelikeBuild  extends Build {
         "ch.qos.logback" % "logback-classic" % "0.9.26",
         "com.weiglewilczek.slf4s" %% "slf4s" % "1.0.7",
         "javax.persistence" % "persistence-api" % "1.0",
-        "com.google.appengine" % "appengine-api-1.0-sdk" % "1.6.5",
         "org.scalatest" %% "scalatest" % "1.6.1" % "test",
+        "com.google.appengine" % "appengine-api-1.0-sdk" % "1.8.1" % "provided",
         "javax.servlet" % "servlet-api" % "2.3" % "provided",
         "org.mortbay.jetty" % "jetty" % "6.1.22" % "container")
     )
     .settings(appengineSettings: _*)
     .settings(scalariformSettings: _*)
     .settings(Twirl.settings: _*)
-    .settings(wro4jSettings: _*)
     .settings(
       Twirl.twirlImports := Seq("uk.co.bruntonspall.sevenday.model._")
     )
--- project/plugins.sbt
+++ project/plugins.sbt
@@ -2,16 +2,14 @@ libraryDependencies <+= sbtVersion(v => "com.github.siasia" %% "xsbt-web-plugin"
 
 resolvers ++= Seq(
   "Scala Tools" at "https://oss.sonatype.org/content/groups/scala-tools",
-  "spray repo" at "http://repo.spray.cc"
+  "spray repo" at "http://repo.spray.cc",
+  Resolver.url("sbt-plugin-snapshots",
+  new URL("http://scalasbt.artifactoryonline.com/scalasbt/sbt-plugin-releases/"))(
+    Resolver.ivyStylePatterns)
 )
 
-resolvers += Resolver.url("sbt-plugin-snapshots",
-  new URL("http://scalasbt.artifactoryonline.com/scalasbt/sbt-plugin-snapshots/"))(
-    Resolver.ivyStylePatterns)
 
-addSbtPlugin("com.bowlingx" %% "xsbt-wro4j-plugin" % "0.1.0-SNAPSHOT")
-
-addSbtPlugin("com.eed3si9n" % "sbt-appengine" % "0.4.1")
+addSbtPlugin("com.eed3si9n" % "sbt-appengine" % "0.5.0")
 
 addSbtPlugin("com.typesafe.sbt" % "sbt-scalariform" % "1.0.0")
 
